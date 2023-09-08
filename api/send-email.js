2c682fb6cc0e2fbd174fddcc389854a1c3d58ae4
muhammad-awan0 <134178770+muhammad-awan0@users.noreply.github.com>
2023-09-08 15:08:35 -0700

    More bug fixes

--- api/send-email.js
+++ api/send-email.js
@@ -1,15 +1,12 @@
-import express from 'express'
-import cors from 'cors'
-import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const app = express();
-app.use(cors());
+export default async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).end();
+  }
 
-app.use(express.json()); 
-
-app.post('/api/send-email', async (req, res) => {
   const { name, email, message } = req.body;
 
   let transporter = nodemailer.createTransport({
@@ -22,23 +19,15 @@ app.post('/api/send-email', async (req, res) => {
 
   let mailOptions = {
     from: email,
-    to: process.env.EMAIL , 
+    to: process.env.EMAIL, 
     subject: `Contact form submission from ${name} and ${email}`,
     text: message
   };
 
   try {
-    console.log("Sending mail with the following options:", mailOptions);
     await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully");
     res.status(200).send('Email sent successfully');
   } catch (error) {
-    console.error("Failed to send email: ", error);
     res.status(500).send('Failed to send email');
   }
-});
-
-const port = 3001;
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+};
