d87689a85e22caf77e27a9dc73f096a83deca6d3
muhammad-awan0 <134178770+muhammad-awan0@users.noreply.github.com>
2023-09-08 15:53:14 -0700

    Made contact form better

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -41,16 +41,16 @@ interface FormData {
     return (
       <div id="contact-section" className="h-[800px] mt-40 ml-12 md:ml-[221.7px]">
         <h1 className="text-[#00E19E] font-bold text-[25px]">🤝 Contact Me</h1>
-        <div className='p-20 bg-slate-400 w-[600px] mt-24 ml-80 rounded-xl border'>
-            <form onSubmit={handleSubmit} className='flex flex-col w-full bg-slate-400'>
-              <input type="text" name="name" placeholder="  Name" onChange={handleChange} required className='h-10 rounded-xl'/>
-              <input type="email" name="email" placeholder="  Email" onChange={handleChange} required className='mt-10 h-10 rounded-xl'/>
-              <textarea name="message" placeholder="  Your message" onChange={handleChange} required className='mt-10 h-40 rounded-xl'></textarea>
-              <button type="submit" className='mt-10'>Send</button>
+        <div className='p-20 bg-[#0F0F37] w-[600px] mt-24 ml-80 rounded-xl border-[#0F0F37] shadow-xl'>
+            <form onSubmit={handleSubmit} className='flex flex-col w-full bg-[#0F0F37]'>
+              <input type="text" name="name" placeholder="Name" onChange={handleChange} required className='text-white bg-[#0F0F37] border-b-2 outline-none border-b-white h-10 focus:border-[#9055A2] focus:border-l-0 focus:border-t-0 focus:border-r-0 transition-all duration-500 ease-linear'/>
+              <input type="email" name="email" placeholder="Email" onChange={handleChange} required className='text-white mt-10 h-10 bg-[#0F0F37] border-b-2 outline-none border-b-white focus:border-[#9055A2] focus:border-l-0 focus:border-t-0 focus:border-r-0 transition-all duration-500 ease-linear'/>
+              <textarea name="message" placeholder="Your message" onChange={handleChange} required className='mt-10 h-40 bg-cyan-50 outline-none rounded-xl pl-3 pt-3'></textarea>
+              <button type="submit" className='mt-10 text-cyan-400 border border-cyan-400 p-3'>Send</button>
             </form>
         </div>
       </div>
-    );  
+    ); 
   };
   
   export default Contact;
