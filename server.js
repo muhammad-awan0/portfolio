4e89a3ad6889ffe0171ed2f2f14f609f0a428267
muhammad-awan0 <134178770+muhammad-awan0@users.noreply.github.com>
2023-09-08 14:53:43 -0700

    More server bug fixes

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: 'muhammad-awan.vercel.app'
-}));
+app.use(cors());
+
 app.use(express.json()); 
 
 app.post('/api/send-email', async (req, res) => {
@@ -29,9 +28,12 @@ app.post('/api/send-email', async (req, res) => {
   };
 
   try {
+    console.log("Sending mail with the following options:", mailOptions);
     await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
     res.status(200).send('Email sent successfully');
   } catch (error) {
+    console.error("Failed to send email: ", error);
     res.status(500).send('Failed to send email');
   }
 });
